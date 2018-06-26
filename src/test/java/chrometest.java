765fb44e5f9397feb83705d947ac06bb8d34b10b
Haile Asnake <hasnake@sbgtv.com>
2018-06-26 16:45:25 -0700

    first commit

---
+++ src/test/java/chrometest.java
@@ -0,0 +1,21 @@
+//import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+public class chrometest {
+ public static void main(String[] args) {
+  System.setProperty("webdriver.chrome.driver","/Users/hasnake/DriverChrome/chromedriver");
+  WebDriver driver = new ChromeDriver();
+  driver.get("http://komonews.com/watch");
+  driver.close();
+  driver.quit();
+ }
+}
+
+
+/*
+Illegal state Exception
+The path to the driver executable must be set by the webdriver.chrome.driver system property; for more information
+needs system.setProperty("webdriver.chrome.driver","path")
+*/
+
