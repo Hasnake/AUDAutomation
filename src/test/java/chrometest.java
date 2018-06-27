79d8adae8b8c7be387751caa381fa72ce4396704
Haile Asnake <hasnake@sbgtv.com>
2018-06-26 17:11:44 -0700

    Adding chrom browser

--- src/test/java/chrometest.java
+++ src/test/java/chrometest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.*;
 
 public class chrometest {
  public static void main(String[] args) {
-  System.setProperty("webdriver.chrome.driver","/Users/hasnake/DriverChrome/chromedriver");
+  System.setProperty("webdriver.chrome.driver","/Users/hasnake/Driver/chromedriver");
   WebDriver driver = new ChromeDriver();
   driver.get("http://komonews.com/watch");
   driver.close();
